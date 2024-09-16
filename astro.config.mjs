a3801da24538d7f08d6b717af5f313cdaa9baa71
KaustubhRai <raikaustubh12@gmail.com>
2024-09-16 16:04:52 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import AutoImport from 'astro-auto-import';
+import sitemap from 'astro-sitemap';
 
 import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks';
 
 export default defineConfig({
+  site: 'https://raikaustubh.com/',
   integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      // Additional options if needed
+    }),
     AutoImport({
       imports: [
         // Update the path if necessary to match the actual location
